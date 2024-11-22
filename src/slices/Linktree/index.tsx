588e6600f9341658a9870ea5a5f170634f369d01
Raul Sigoli <raul_sigoli@hotmail.com>
2024-11-22 17:33:50 -0300

    fix: testando a homepage

--- src/slices/Linktree/index.tsx
+++ src/slices/Linktree/index.tsx
@@ -2,36 +2,48 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import * as S from "./styles";
-import { PrismicNextImage } from "@prismicio/next";
 
 type LinktreeProps = SliceComponentProps<Content.LinktreeSlice>;
 
 const Linktree = ({ slice }: LinktreeProps): JSX.Element => {
+
+  if (!slice.primary) {
+    return <div>Carregando...</div>;
+  }
+
   return (
     <>
-      <S.GlobalStyle />
+      {/* <S.GlobalStyle /> */}
 
       <S.Container>
-        {/* <S.Image id="Photo" src="assets/Vivek.jpg" alt="VivekArtiste" /> */}
-        <S.Imagem src={slice.primary.foto.url || ""} alt={slice.primary.foto.alt || ""} />
-        <S.Nome>
-          {slice.primary.nome}
+        {slice.primary.foto.url && (
+          <S.Imagem
+            src={slice.primary?.foto?.url || "/placeholder.jpg"}
+            alt={slice.primary?.foto?.alt || "Imagem padrão"}
+          />
+        )}
 
-          <S.NomeSub>
-            {slice.primary.clinica_estetica}
-          </S.NomeSub>
-        </S.Nome>
-        
+        {slice.primary.nome && (
+          <S.Nome>
+            {slice.primary.nome}
+            {slice.primary.clinica_estetica && (
+              <S.NomeSub>{slice.primary.clinica_estetica}</S.NomeSub>
+            )}
+          </S.Nome>
+        )}
 
-
-
-
-
-        {slice.primary.links.map((item, index) => (
-          <S.LinkItem key={index} href={item.link_do_texto.text || ""} target="_blank" rel="noopener noreferrer">
-            {item.textolink}
-          </S.LinkItem>
-        ))}
+        {slice.primary.links?.map((item, index) =>
+          item.link_do_texto.text ? (
+            <S.LinkItem
+              key={index}
+              href={item.link_do_texto.text}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.textolink || "Texto do link"}
+            </S.LinkItem>
+          ) : null
+        )}
       </S.Container>
     </>
   );
